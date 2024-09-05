40a34a0628e61ae99b15ed59720d2ec83b0a6a5f
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-09-05 12:12:20 +0300

    basics

---
+++ Lab 2 Dart Fundamentals/Tutorial/basics.dart
@@ -0,0 +1,12 @@
+// variable declaration and data types in dart
+
+void main(List<String> args) {
+  int age = 20;
+  String name = 'Alex';
+
+  print("Name is " + name + " and age is " + age.toString());
+  print("Name is $name and age is $age");
+
+
+  
+}
