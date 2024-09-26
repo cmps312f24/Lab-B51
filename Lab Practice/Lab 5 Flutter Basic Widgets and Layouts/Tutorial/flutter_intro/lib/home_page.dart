e3561639eca4a19238465671b95c2bc852cce1f7
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-09-26 13:19:44 +0300

    Update home_page.dart

--- Lab Practice/Lab 5 Flutter Basic Widgets and Layouts/Tutorial/flutter_intro/lib/home_page.dart
+++ Lab Practice/Lab 5 Flutter Basic Widgets and Layouts/Tutorial/flutter_intro/lib/home_page.dart
@@ -6,9 +6,54 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: const Center(
-        child: Text("Welcome to Flutter Development"),
+      color: Colors.purpleAccent,
+      child: const SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "CMPS 312",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                  backgroundColor: Colors.yellow,
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(bottom: 50.0),
+              child: Center(
+                child: Text(
+                  "Welcome",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      decoration: TextDecoration.none),
+                ),
+              ),
+            ),
+            // SizedBox(
+            //   height: 50,
+            // ),
+            Center(
+              child: Text(
+                "Ali",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                  backgroundColor: Colors.yellow,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
