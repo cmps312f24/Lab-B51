5e7864f51bb6549dc954560f1cf70e38e53e5a05
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-09-26 14:16:58 +0300

    Update home_page.dart

--- Lab Practice/Lab 5 Flutter Basic Widgets and Layouts/Tutorial/flutter_intro/lib/home_page.dart
+++ Lab Practice/Lab 5 Flutter Basic Widgets and Layouts/Tutorial/flutter_intro/lib/home_page.dart
@@ -5,45 +5,69 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.brown[400],
-      child: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 50),
-              child: Center(
-                child: Text(
-                  "CMPS 312",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                    decoration: TextDecoration.none,
-                    backgroundColor: Colors.yellow,
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Home Page",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.purple,
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
+        actions: const [
+          Icon(
+            Icons.notifications,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 20,
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 50),
+            child: Center(
+              child: Text(
+                "CMPS 312",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                  backgroundColor: Colors.yellow,
                 ),
               ),
             ),
+          ),
 
-            const Padding(
-              padding: EdgeInsets.only(bottom: 50.0),
-              child: Center(
-                child: Text(
-                  "Welcome",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                      decoration: TextDecoration.none),
-                ),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 50.0),
+            child: Center(
+              child: Text(
+                "Welcome",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    // color: Colors.white,
+                    decoration: TextDecoration.none),
               ),
             ),
-            // SizedBox(
-            //   height: 50,
-            // ),
-            const Center(
+          ),
+          // SizedBox(
+          //   height: 50,
+          // ),
+          const Card(
+            child: Center(
               child: Text(
                 "Ali",
                 textAlign: TextAlign.center,
@@ -55,17 +79,58 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            const Icon(
-              Icons.person,
-              size: 60,
-              color: Colors.white,
-            ),
+          ),
+          const Icon(
+            Icons.person,
+            size: 60,
+            // color: Colors.white,
+          ),
+          SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              )),
 
-            Image.asset("images/happy_cat.jpg")
-            // Image.network(
-            //     "https://static.vecteezy.com/system/resources/thumbnails/029/554/988/small_2x/surprised-cat-scottish-isolated-on-white-background-generative-ai-photo.jpg"),
-          ],
-        ),
+          const Card(
+            elevation: 40,
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Enter your name",
+                labelText: "Name",
+                labelStyle: TextStyle(color: Colors.purple),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+            ),
+          ),
+          const Switch(
+            value: true,
+            onChanged: null,
+            activeTrackColor: Colors.amber,
+            inactiveTrackColor: Colors.blue,
+          ),
+          // Image.asset("images/happy_cat.jpg")
+          // Image.network(
+          //     "https://static.vecteezy.com/system/resources/thumbnails/029/554/988/small_2x/surprised-cat-scottish-isolated-on-white-background-generative-ai-photo.jpg"),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
       ),
     );
   }
